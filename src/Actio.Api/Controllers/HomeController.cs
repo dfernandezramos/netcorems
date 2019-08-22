c9be1c26bdd2dd617de15ba8d9f25400fefac1a5
dfernandezramos <dfz.ramos@gmail.com>
2019-08-22 20:15:31 +0200

    Implement API Endpoints

---
+++ src/Actio.Api/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Actio.Api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Content("Hello from Actio API!");
+        }
+    }
+}
---
+++ src/Actio.Api/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Actio.Common.Commands;
+using Microsoft.AspNetCore.Mvc;
+using RawRabbit;
+using System;
+using System.Threading.Tasks;
+
+namespace Actio.Api.Controllers
+{
+    [Route("[controller1]")]
+    public class UsersController : Controller
+    {
+        IBusClient _busClient;
+
+        public UsersController(IBusClient busClient)
+        {
+            _busClient = busClient;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Post([FromBody]CreateUser command)
+        {
+            await _busClient.PublishAsync(command);
+
+            return Accepted();
+        }
+    }
+}
