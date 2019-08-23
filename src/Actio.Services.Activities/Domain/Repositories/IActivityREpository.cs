4124d986a0b78cd04653899f5d6c97fb6b3b6208
dfernandezramos <dfz.ramos@gmail.com>
2019-08-23 19:32:07 +0200

    Creating domain models

---
+++ src/Actio.Services.Activities/Domain/Repositories/IActivityREpository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Actio.Services.Activities.Domain.Repositories
+{
+    interface IActivityREpository
+    {
+        Task<Activity> GetAsync(Guid id);
+
+        Task AddAsync(Activity category);
+    }
+}
