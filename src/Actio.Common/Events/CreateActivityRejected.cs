af61b1aae3600801388054cd0c6043e19e443da1
dfernandezramos <dfz.ramos@gmail.com>
2019-09-02 15:00:51 +0200

    Implementing handlers

--- src/Actio.Common/Events/CreateActivityRejected.cs
+++ src/Actio.Common/Events/CreateActivityRejected.cs
@@ -5,29 +5,20 @@ namespace Actio.Common.Events
     public class CreateActivityRejected : IRejectedEvent
     {
 
-        public Guid UserId { get; set; }
+        public Guid Id { get; set; }
 
-        public string Category { get; set; }
-
-        public string Name { get; set; }
-
-        public string Description { get; set; } 
-
-        public string Reason { get; }  
+        public string Reason { get; }
 
         public string Code { get; }
 
-        protected CreateActivityRejected ()
+        protected CreateActivityRejected()
         {
 
         }
 
-        public CreateActivityRejected (Guid userId, string category, string name, string description, string reason, string code)
+        public CreateActivityRejected(Guid id, string reason, string code)
         {
-            UserId = userId;
-            Category = category;
-            Name = name;
-            Description = description;
+            Id = id;
             Reason = reason;
             Code = code;
         }
--- src/Actio.Services.Activities/Handlers/CreateActivityHandler.cs
+++ src/Actio.Services.Activities/Handlers/CreateActivityHandler.cs
@@ -1,5 +1,8 @@
 ﻿using Actio.Common.Commands;
 using Actio.Common.Events;
+using Actio.Common.Exceptions;
+using Actio.Services.Activities.Services;
+using Microsoft.Extensions.Logging;
 using RawRabbit;
 using System;
 using System.Threading.Tasks;
@@ -8,17 +11,34 @@ namespace Actio.Services.Activities.Handlers
 {
     public class CreateActivityHandler : ICommandHandler<CreateActivity>
     {
-        IBusClient _busClient;
+        readonly IBusClient _busClient;
+        readonly IActivityService _activityService;
+        readonly ILogger _logger;
 
-        public CreateActivityHandler(IBusClient busClient)
+        public CreateActivityHandler(IBusClient busClient, IActivityService activityService, ILogger<CreateActivityHandler> logger)
         {
             _busClient = busClient;
+            _activityService = activityService;
+            _logger = logger;
         }
 
         public async Task HandleAsync(CreateActivity command)
         {
-            Console.WriteLine($"Creating activity: {command.Name}");
-            await _busClient.PublishAsync(new ActivityCreated(command.Id, command.UserId, command.Category, command.Name, command.Description, command.CreatedAt));
+            _logger.LogInformation($"Creating activity: {command.Name}");
+
+            try
+            {
+                await _activityService.AddAsync(command.Id, command.UserId, command.Category, command.Name, command.Description, command.CreatedAt);
+                await _busClient.PublishAsync(new ActivityCreated(command.Id, command.UserId, command.Category, command.Name, command.Description, command.CreatedAt));
+
+                return;
+            }
+            catch (ActioException ex)
+            {
+                await _busClient.PublishAsync(new CreateActivityRejected(command.Id, "error", ex.Message));
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
