239f48d46a6f8ce3a16c608ec9aeee450625910f
dfernandezramos <dfz.ramos@gmail.com>
2019-09-03 19:20:30 +0200

    Implementing JWT with HMAC

---
+++ src/Actio.Common/Auth/JwtHandler.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Actio.Common.Auth
+{
+    public class JwtHandler : IJwtHandler
+    {
+        readonly JwtSecurityTokenHandler _jwtSecurityTokenHandler = new JwtSecurityTokenHandler();
+        readonly JwtOptions _options;
+        readonly SecurityKey _issuerSigningKey;
+        readonly SigningCredentials _signingCredentials;
+        readonly JwtHeader _jwtHeader;
+        readonly TokenValidationParameters _tokenValidationParameters;
+
+        public JwtHandler(IOptions<JwtOptions> options)
+        {
+            _options = options.Value;
+            _issuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_options.SecretKey));
+            _signingCredentials = new SigningCredentials(_issuerSigningKey, SecurityAlgorithms.HmacSha256);
+            _jwtHeader = new JwtHeader(_signingCredentials);
+            _tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidIssuer = _options.Issuer,
+                IssuerSigningKey = _issuerSigningKey
+            };
+        }
+
+        public JsonWebToken Create(Guid userId)
+        {
+            var nowUtc = DateTime.UtcNow;
+            var expires = nowUtc.AddMinutes(_options.ExpiryMinutes);
+            var centuryBegin = new DateTime(1970, 1, 1).ToUniversalTime();
+            var exp = (long)(new TimeSpan(expires.Ticks - centuryBegin.Ticks).TotalSeconds);
+            var now = (long)(new TimeSpan(nowUtc.Ticks - centuryBegin.Ticks).TotalSeconds);
+            var payload = new JwtPayload
+            {
+                { "sub", userId },
+                { "iss", _options.Issuer },
+                { "iat", now },
+                { "exp", exp },
+                { "unique_name", userId },
+            };
+            var jwt = new JwtSecurityToken(_jwtHeader, payload);
+            var token = _jwtSecurityTokenHandler.WriteToken(jwt);
+
+            return new JsonWebToken
+            {
+                Token = token,
+                Expires = exp
+            };
+        }
+    }
+}
